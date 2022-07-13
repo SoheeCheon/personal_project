e89ad48f6a5f1971e2644aadf54605cce567a8c5
Yenno <csh5156@gmail.com>
2022-07-13 18:42:55 +0900

    공부중

---
+++ vue3/vue3-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>{{ name }}</div>
+  <input type="text" v-model="name">
+  <button class="btn btn-primary" @click="onSumbit">Click</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+// import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const name = ref('Sohee')
+
+    const onSumbit = () => {
+      console.log(name.value)
+    }
+    
+    return {
+      name,
+      onSumbit,
+    }
+  },
+}
+</script>
+
+<style>
+.name {
+  color: red;
+}
+
+.blue {
+  color: blue
+}
+</style>
